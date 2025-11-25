cbe4756060eba6aaf471d2946364df78206dc0e5
Baohang Li <guaguagua.baohang@gmail.com>
2025-11-25 11:46:54 -0500

    Added training and testing functions to CNN.py

---
+++ CNN.py
@@ -0,0 +1,29 @@
+
+def forwardProp(network, images):
+    for layer in network:
+        images = layer.forward(images)
+    return images
+
+def backwardProp(network, gradient):
+    for layer in reversed(network):
+        gradient = layer.backward(gradient)
+    return gradient
+
+def trainCNN(network, loss, lossGradient, xTrain, yTrain, epochs = 5, verbose = True):
+    for epoch in range(epochs):     
+        prediction = forwardProp(network, xTrain)
+
+        error = loss(prediction, yTrain)
+
+        backwardProp(network, lossGradient(prediction, yTrain))
+
+        print(f"Training error on epoch {epoch} is {error}")
+    
+    return network
+
+def testCNN(network, loss, xTest, yTest):
+    prediction = forwardProp(network, xTest)
+
+    error = loss(prediction, yTest)
+
+    print(f"Testing error is {error}")
